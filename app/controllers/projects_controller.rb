3cd650b3fd8b880b4b6adbf1df9df3c08b6975d8
Said Maadan <saidmaadan@users.noreply.github.com>
2015-07-25 13:54:29 -0500

    Implemented carrierwave with AWS S3 integration

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,48 @@
 class ProjectsController < ApplicationController
+
+	def index
+		@projects = Project.all
+	end
+
+
+  def show
+    @project = Project.friendly.find(params[:id])
+  end
+    
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.new(project_params)
+		if @project.save
+			redirect_to @project
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@project = Project.friendly.find(params[:id])
+	end
+
+	def update
+		@project = Project.friendly.find(params[:id])
+		if @project.update(project_params)
+			redirect_to @project, notice: "Project updated successfully"
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@project = Project.friendly.find(params[:id])
+		@project.destroy
+		redirect_to projects_url, notice: "Project has been destroyed successfully"
+	end
+
+	private
+	def project_params
+		params.require(:project).permit(:name, :description, :url, :github, :builtwith, :screenshot)
+	end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ActiveRecord::Base
 	mount_uploader :screenshot, ScreenshotUploader
+
+	extend FriendlyId
+  friendly_id :name, use: :slugged
 end
