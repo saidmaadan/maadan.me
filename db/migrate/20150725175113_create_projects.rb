0425a59b0177b90e7dd807618ac8e0cd1f55ae91
Said Maadan <saidmaadan@users.noreply.github.com>
2015-07-25 13:05:28 -0500

    Initial commit

---
+++ db/migrate/20150725175113_create_projects.rb
@@ -0,0 +1,13 @@
+class CreateProjects < ActiveRecord::Migration
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.text :description
+      t.string :url
+      t.string :github
+      t.string :builtwith
+
+      t.timestamps null: false
+    end
+  end
+end
