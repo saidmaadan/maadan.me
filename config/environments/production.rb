a1e42f51aebbd09fee99aefc787488820d6eb3a9
Said Maadan <saidmaadan@users.noreply.github.com>
2015-07-27 12:11:02 -0500

    Modified environment/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => "maadan.me"}
+  config.action_mailer.default_url_options = { :host => "maadan.herokuapp.com"}
 end
